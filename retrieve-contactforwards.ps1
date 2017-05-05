4494965296cd5d896e9d329ae94980367cd74667
jmf2n <jmf2n@virginia.edu>
2017-05-05 15:51:00 -0400

    retrieves all mailboxes forwarding to a contact object
    pulls all mailboxes forwarded to a contact in the AD and expands the
    target of the contact so that the addresses can be rewritten in the more
    O365 friendly ForwardingSMTPaddress property

---
+++ retrieve-contactforwards.ps1
@@ -0,0 +1,32 @@
+﻿# add the Exchange powershell snapin for on-premises Exchange 2010 environment; may be different for Exchange 2013/2016
+
+Add-PSSnapin Microsoft.Exchange.Management.Powershell.SnapIn
+
+# In this script we will gather all forwarded mailboxes in the organization that are referencing contact objects.  Then we will expand the target of the contacts into the actual email addresses they are forwarding to.
+
+$Mailboxes = Get-Mailbox -resultsize unlimited -filter {ForwardingAddress -ne $null} | select alias,*forward*
+$outputcsv = forwardedmailboxes.csv
+
+# Grab all of the mailboxes that have a forward set and if it's to a contact, the target of the contact
+
+$Results = ForEach($Mailbox in $Mailboxes) {
+$ForwardingAddress = $null
+$ContactDN = $null
+	if($Mailbox.ForwardingAddress -ne $null){
+$Contact = Get-Recipient $Mailbox.ForwardingAddress
+$ForwardingAddress = $Contact.ExternalEmailAddress
+$ContactDN = $Contact.Identity
+}
+elseif ($Mailbox.ForwardingSMTPAddress -ne $null){
+$ForwardingAddress = $Mailbox.ForwardingSMTPAddress}
+
+	New-Object -TypeName PSObject -Property @{
+		SamAccountName = $Mailbox.alias
+        ContactDN = $ContactDN
+        TargetOfForwardingAddress = $ForwardingAddress
+        DeliverToMailboxAndForward =$mailbox.DeliverToMailboxAndForward
+	}
+}
+# Here we can just export the results to the screen or pipe them out to CSV with the usual parameters.
+
+$Results | select samaccountname,contactdn,targetofforwardingaddress,delivertomailboxandforward | export-csv $outputcsv -NoTypeInformation
